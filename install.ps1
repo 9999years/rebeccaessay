cc5a5a233831d735df7e914521e095bdf05c051f
Rebecca Turner <637275@gmail.com>
2018-10-22 22:05:52 -0400

    installation files

---
+++ install.ps1
@@ -0,0 +1,12 @@
+[CmdletBinding()]
+Param(
+	[String] $TexMFRoot = "~/texmf"
+)
+
+$class = "rebeccaessay"
+$dest = (Join-Path $TexMFRoot  "tex/latex/$class")
+If(!(Test-Path $dest)) {
+	mkdir $dest
+}
+
+cp "$class.cls" $dest
