a8b61fd0cec854e1cb779f8614d1894bd1381a3c
Rebecca Turner <637275@gmail.com>
2019-02-27 18:01:36 -0500

    work...

--- install.ps1
+++ install.ps1
@@ -4,13 +4,16 @@ Param(
 )
 
 $class = "rebeccaessay"
+$pkg = "rebeccastyle"
 $dest = (Join-Path $TexMFRoot  "tex/latex/$class")
 If(!(Test-Path $dest)) {
 	mkdir $dest
 }
 
 cp "$class.cls" $dest
+cp "$pkg.sty" $dest
 pushd
 cd ~
 kpsewhich "$class.cls"
+kpsewhich "$pkg.sty"
 popd
